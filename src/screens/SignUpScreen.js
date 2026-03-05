2cfb96e287131f9b0a26f0b6007c7f886a17c1b8
AwesomeWiz <abhishikthsmattam@gmail.com>
2026-03-05 14:14:08 +0530

    change in path

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -108,7 +108,7 @@ export default function SignUpScreen({ navigation }) {
         return;
       }
 
-      const res = await axios.post("http://172.16.2.137:5000/signup", {
+      const res = await axios.post("http://10.141.79.142:5000/signup", {
         store: store,
         phone: phone,
         password: password,
